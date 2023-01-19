5671e416880ba407b12e309df1fd6bf74a96441c
Sergio Otero <a22sergiooc@iessanclemente.net>
2023-01-19 14:28:02 +0100

    Comentarios engadidos e funcion eliminar parcheado

--- Menu.sh
+++ Menu.sh
@@ -39,7 +39,7 @@ function listarContido() { # Esta función mostra o contido do directorio
     sleep 4 # O 'sleep' úsase para esperar un tempo determinado en segundos antes de executar o resto do programa
 }
 
-function crearFicheiro() { # Esta función recolle nunha variable un nome e crea un directorio ou ficheiro co nome recollido
+function crearFicheiro() { # Esta función mostra un menú secundario para crear un directorio ou ficheiro co nome recollido
     echo $limpar
     echo "╔════════════════════════════════════════════════════════════════════════════════════╗"
     echo -e "║                           \033[34mQue queres crear?\033[0m                                        ║"
@@ -48,23 +48,23 @@ function crearFicheiro() { # Esta función recolle nunha variable un nome e crea
     echo "╠════════════════════════════════════════════════════════════════════════════════════╣"
     echo -e "║ \033[1;34m» 2. Un ficheiro. «\033[0m                                                                ║"
     echo "╚════════════════════════════════════════════════════════════════════════════════════╝"
-    read -p "Escolla unha opción: " chooseFile # A variable 'escolla' almacena a opción do menú escollida
+    read -p "Escolla unha opción: " chooseFile # A variable 'chooseFile' almacena a opción do menú escollida (directorio ou ficheiro)
     case $chooseFile in
-        1)
+        1) # No caso de ser un directorio, almacena un nome e crea un directorio con ese nome
             read -p "Introduce o nome do directorio: " folder_name
             echo `mkdir $folder_name`
             echo Directorio creado exitosamente.
             echo -e "\033[31mVolvendo ao menú...\033[0m"
             sleep 2
         ;;
-        2)
+        2) # No caso de ser un ficheiro, almacena un nome e crea un ficheiro con ese nome (e extensión se se engade ao nome)
             read -p "Introduce o nome do ficheiro (coa extensión incluída): " file_name
             echo `touch $file_name`
             echo Ficheiro creado exitosamente.
             echo -e "\033[31mVolvendo ao menú...\033[0m"
             sleep 2
         ;;
-        *)
+        *) # No caso de seleccionar un distinta ás do menú, dá erro e volve ao menú principal
             echo -e "\033[31mError. Introduce unha opción válida.\033[0m"
             echo -e "\033[31mVolvendo ao menú...\033[0m"
             sleep 2
@@ -72,7 +72,7 @@ function crearFicheiro() { # Esta función recolle nunha variable un nome e crea
     esac
 }
 
-function imprimirHelloWorld() {
+function imprimirHelloWorld() { # Esta función mostra un menú secundario para imprimir un Hello World en diferentes estilos ASCII
     echo $limpar
     echo "╔════════════════════════════════════════════════════════════════════════════════════╗"
     echo -e "║                           \033[34mQue estilo prefires?\033[0m                                     ║"
@@ -83,8 +83,8 @@ function imprimirHelloWorld() {
     echo "╠════════════════════════════════════════════════════════════════════════════════════╣"
     echo -e "║ \033[1;34m» 3. 3D ASCII. «\033[0m                                                                   ║"
     echo "╚════════════════════════════════════════════════════════════════════════════════════╝"
-    read -p "Escolla unha opción: " chooseHW
-    case $chooseHW in
+    read -p "Escolla unha opción: " chooseHW # A variable 'chooseHW' almacena a opción escollida
+    case $chooseHW in # Dependendo da opción escollida, imprime un estilo ou outro
         1)
             echo "  _    _      _ _        __          __        _     _ "
             echo " | |  | |    | | |       \ \        / /       | |   | |"
@@ -117,7 +117,7 @@ function imprimirHelloWorld() {
             echo -e "\033[31mVolvendo ao menú...\033[0m"
             sleep 3
         ;;
-        *)
+        *) # No caso de seleccionar un distinta ás do menú, dá erro e volve ao menú principal
             echo -e "\033[31mError. Introduce unha opción válida.\033[0m"
             echo -e "\033[31mVolvendo ao menú...\033[0m"
             sleep 2
@@ -135,36 +135,39 @@ function engadirLinha() { #Esta función recolle unha liña e añade a liña a u
         sleep 2
 }
 
-function eliminarArquivo() {
-    read -p "Escribe o nome do arquivo a eliminar: " dfile_name
-        read -p "Estás seguro de querer eliminar $dfile_name ? (s/n) " confirmfile
-        if [ $confirmfile == s ]
+function eliminarArquivo() { #Esta función serve para eliminar un arquivo ou directorio xa existente
+    read -p "Escribe o nome do arquivo a eliminar: " dfile_name # Recolle o nome do ficheiro
+    if [ -e $dfile_name ] # Aquí comproba se o arquivo a eliminar existe. Se, en efecto, existe, procede a borrar o arquivo.
+    then
+        read -p "Estás seguro de querer eliminar $dfile_name ? (s/n) " confirmfile # Pregunta outra vez se se quere eliminar, por cuestións de seguridade
+        if [ $confirmfile == s ] # Se se volve a confirmar, elimínase e volve ao menú principal
         then
-            if [ $dfile_name == "Menu.sh" ] 
-            then
+            if [ $dfile_name == "Menu.sh" ] # Comproba se o arquivo a eliminar é o propio script. Nese caso, automáticamente dá erro e cancélase a acción
+            then                            # Este proceso faise por razóns obvias. Se se elimina o script, xa non se poderá volver a executar.
                 echo -e "\033[31mError. Non se pode usar o programa para borrar o propio programa.\033[0m"
                 echo -e "\033[31mVolvendo ao menú...\033[0m"
-            elif [ -e $dfile_name ]
-            then
-                echo `rm -r $dfile_name`
-                echo Arquivo eliminado eliminado exitosamente.
-                echo -e "\033[31mVolvendo ao menú...\033[0m"
             else
-                echo -e "\033[31mError. O arquivo a eliminar non existe.\033[0m"
-                echo -e "\033[31mVolvendo ao menú...\033[0m"
+                echo `rm -r $dfile_name`
+                echo Arquivo eliminado exitosamente.
+                echo -e "\033[31mVolvendo ao menú...\033[0m" 
+                
             fi
-        else
+        else # Se ao volver a preguntar se decide non eliminar o arquivo, a acción cancélase e vólvese ao menú principal
             echo Acción cancelada.
             echo -e "\033[31mVolvendo ao menú...\033[0m"
         fi
-        sleep 2
+    else # No caso de que o arquivo non exista, dá erro, a acción cancélase e volve ao menú principal
+        echo -e "\033[31mError. O arquivo a eliminar non existe.\033[0m"
+        echo -e "\033[31mVolvendo ao menú...\033[0m"
+    fi
+    sleep 2
 }
 
-until (( $sair == 1 ))
-do
+until (( $sair == 1 )) # Este bucle serve para usar o menú varias veces. Sin este bucle, ao escoller unha opción e executala, saldría do programa automáticamente,
+do                     # e habería que volver a executar o programa.
     mostrarMenu
 
-    case $escolla in
+    case $escolla in # Dependendo da opción escollida no menú principal, executa a función correspondente.
         1)
         listarContido
         ;;
@@ -180,12 +183,12 @@ do
         5)
         eliminarArquivo
         ;;
-        *)
+        *) # No caso de seleccionar un distinta ás do menú, o bucle acaba e sae do programa.
         sair=1
         ;;
     esac
 done
 
-echo "Cerrando programa. Hasta outra!"
+echo "Cerrando programa. Hasta outra!" # Ao saír do programa, mostra unha mensaxe de despedida.
 sleep 2
-echo $limpar
+echo $limpar # Ao final, limpa a terminal.
