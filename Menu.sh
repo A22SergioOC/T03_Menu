5e98c7c80c2ce6e6a8d1f14ae8eaa6a5c4ef7d9a
Sergio Otero <a22sergiooc@iessanclemente.net>
2023-01-19 10:09:45 +0100

    Menu actualizado e funciones feitas

--- Menu.sh
+++ Menu.sh
@@ -8,68 +8,48 @@ sair=0
 
 function mostrarMenu() {
 echo $limpar
-echo Escolla unha opción:
-
-echo "Prema 1 para listar o contido do directorio."
-
-echo "Prema 2 para crear un directorio."
-
-echo "Prema 3 para crear un ficheiro."
-
-echo "Prema 4 para engadir un liña a un ficheiro (se non existe o ficheiro, créao)"
-
-echo "Prema 5 para borrar un ficheiro ou directorio."
-
-echo "Prema calquera outra tecla para cerrar o menú."
+echo "╔════════════════════════════════════════════════════════════════════════════════════╗"
+echo -e "║                           \033[34mBenvido ao xestor de ficheiros.\033[0m                          ║"
+echo "╠════════════════════════════════════════════════════════════════════════════════════╣"
+echo "║ » Prema 1 para listar o contido do directorio. «                                   ║"
+echo "╠════════════════════════════════════════════════════════════════════════════════════╣"
+echo "║ » Prema 2 para crear un directorio. «                                              ║"
+echo "╠════════════════════════════════════════════════════════════════════════════════════╣"
+echo "║ » Prema 3 para crear un ficheiro. «                                                ║"
+echo "╠════════════════════════════════════════════════════════════════════════════════════╣"
+echo "║ » Prema 4 para engadir un liña a un ficheiro (se non existe o ficheiro, créao) «   ║"
+echo "╠════════════════════════════════════════════════════════════════════════════════════╣"
+echo "║ » Prema 5 para borrar un ficheiro ou directorio. «                                 ║"
+echo "╠════════════════════════════════════════════════════════════════════════════════════╣"
+echo "║ » Prema calquera outra tecla para cerrar o menú. «                                 ║"
+echo "╚════════════════════════════════════════════════════════════════════════════════════╝"
+read -p "Escolla unha opción: " escolla
 }
 
-echo Benvido ao xestor de ficheiros.
-read -p "Para comezar, introduce a carpeta sobre a que traballar: " folder
+function crearDirectorio() {
+    read -p "Introduce o nome do directorio: " folder_name
+    echo `mkdir $folder_name`
+    echo Directorio creado exitosamente.
+    sleep 2
+}
 
-if [ -z $folder ]
-then
-    folder=`$HOME/*`
-fi
-
-if [ -e $folder ]
-then
-    echo `mkdir $folder`
-fi
-
-echo `cd $folder`
-
-until (( $sair == 1 ))
-do
-    mostrarMenu
-
-    read escolla
-
-    case $escolla in
-        1)
-        echo `ls`
-        sleep 5
-        ;;
-        2)
-        read -p "Introduce o nome do directorio: " folder_name
-        echo `mkdir $folder_name`
-        echo Directorio creado exitosamente.
-        sleep 3
-        ;;
-        3)
-        read -p "Introduce o nome do ficheiro (coa extensión incluída): " file_name
+function crearFicheiro() {
+    read -p "Introduce o nome do ficheiro (coa extensión incluída): " file_name
         echo `touch $file_name`
         echo Ficheiro creado exitosamente.
-        sleep 3
-        ;;
-        4)
-        read -p "Introduce o nome do ficheiro (coa extensión incluída): " file_name_edit
+        sleep 2
+}
+
+function engadirLinha() {
+    read -p "Introduce o nome do ficheiro (coa extensión incluída): " file_name_edit
         read -p "Escribe a liña a engadir: " linha
         echo `echo $linha >> $file_name_edit`
         echo Ficheiro modificado exitosamente.
-        sleep 3
-        ;;
-        5)
-        read -p "Escribe o nome do arquivo a eliminar: " dfile_name
+        sleep 2
+}
+
+function eliminarArquivo() {
+    read -p "Escribe o nome do arquivo a eliminar: " dfile_name
         read -p "Estás seguro de querer eliminar $dfile_name ? (s/n) " confirmfile
         if [ $confirmfile == s ]
         then
@@ -78,7 +58,29 @@ do
         else
             echo Acción cancelada.
         fi
-        sleep 3
+        sleep 2
+}
+
+until (( $sair == 1 ))
+do
+    mostrarMenu
+
+    case $escolla in
+        1)
+        echo `ls`
+        sleep 4
+        ;;
+        2)
+        crearDirectorio
+        ;;
+        3)
+        crearFicheiro
+        ;;
+        4)
+        engadirLinha
+        ;;
+        5)
+        eliminarArquivo
         ;;
         *)
         sair=1
@@ -87,4 +89,4 @@ do
 done
 
 echo "Cerrando programa. Hasta outra!"
-sleep 3
+sleep 2
